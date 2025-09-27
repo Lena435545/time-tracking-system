d5eb6b6fc0eacfcab4d588c667c173793e4f5e8f
Lena435545 <manichevalena8@gmail.com>
2025-09-27 08:39:14 +0200

    revert changes

---
+++ src/src/main/java/com/manicheva/TimeTrackingSystem/config/SecurityConfig.java
@@ -0,0 +1,54 @@
+package com.manicheva.TimeTrackingSystem.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity(prePostEnabled = true)
+public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                          .requestMatchers("/admin/**").hasRole("ADMIN")
+                          .requestMatchers("/auth/login", "/auth/registration", "/error", "/css/**", "/images/**").permitAll()
+                          .anyRequest().authenticated()
+                  )
+                  .formLogin(form -> form
+                          .loginPage("/auth/login")
+                          .loginProcessingUrl("/process_login")
+                          .defaultSuccessUrl("/", true)
+                          .failureUrl("/auth/login?error")
+                          .permitAll()
+                  )
+                  .logout(logout -> logout
+                          .logoutUrl("/auth/logout")
+                          .logoutSuccessUrl("/auth/login")
+                          .permitAll()
+                  )
+                  .exceptionHandling(ex -> ex
+                          .accessDeniedPage("/auth/access_denied"));
+                //todo login settings turned off for testing
+                /*.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
+                .formLogin(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .logout(AbstractHttpConfigurer::disable);*/
+
+        return http.build();
+    }
+}
---
+++ src/src/test/java/com/manicheva/TimeTrackingSystem/TimeTrackingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manicheva.TimeTrackingSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeTrackingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
